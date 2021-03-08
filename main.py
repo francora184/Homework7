0a4ce5481f87186eef2e4b243148aef896d1e9fa
francora <francora@oregonstate.edu>
2021-03-08 00:04:38 +0000

    Wrote first test, test checks to see if a year divisible by 4 is a leap year

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ def fizzBuzz(x):
     return "Fizz"
   if(x % 5 == 0):
     return "Buzz"
+
+def leapYear(y):
+  if (y % 4 == 0):
+   if (y % 100 == 0):
+       if (y % 400 == 0):
+          return "Yes"
+       else:
+          return "No"
+   else:
+      return "Yes"
+  else:
+    return "No"
--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import pytest
 
 from main import fizzBuzz
+from main import leapYear
 
 def test_fizz():
     assert fizzBuzz(3) == "Fizz"
     assert fizzBuzz(6) == "Fizz"
     assert fizzBuzz(10) == "Buzz"
     assert fizzBuzz(15) == "FizzBuzz"
+
+def test_leapyear():
+    assert leapYear(2000) == "Yes"
+    
 
